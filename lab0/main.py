b85a8e60474fff0978ae65dfbbae95b37ec6ab45
Ilona Urbanovich <like.ilonochka@gmail.com>
2026-02-15 09:56:31 +0200

    Lab0: Structure and Hello World

---
+++ lab0/main.py
@@ -0,0 +1,12 @@
+# Лабораторна робота №0
+# Виконав: [Ваше Прізвище]
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print("Hello, world! Робоче середовище налаштовано.")
+    print(f"NumPy версія: {np.__version__}")
+
+if __name__ == "__main__":
+    main()
